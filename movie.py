d689ebd33d18b2b0f72947c1b782a99179314b9c
Wirelessr <ifloveyou2008@gmail.com>
2017-12-25 05:59:38 +0000

    Persisting Data with SQLAlchemy ORM

--- movie.py
+++ movie.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Integer, Date, Table, ForeignKey
-from sqlalchemy import relationship
+from sqlalchemy.orm import relationship
 
 from base import Base
 
@@ -15,7 +15,7 @@ class Movie(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String)
     release_date = Column(Date)
-	actors = relationship("Actor", secondary=movies_actors_association)
+    actors = relationship("Actor", secondary=movies_actors_association)
 
     def __init__(self, title, release_date):
         self.title = title
